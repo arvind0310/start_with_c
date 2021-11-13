7481536056086f3c3be8914935e05ab4df5582a7
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-11-13 18:23:20 +0530

    Update f1.c

--- pointer/f1.c
+++ pointer/f1.c
@@ -1,27 +1,23 @@
 
-
 #include <stdio.h>
 
 int main()
 {
  int x=5;
  int *p=&x; // p will have add of x.
-  
- printf("Add of x is:%p\n",&x);
- printf("add of x is:%p",p);
-
  
  printf("the vlue of x is: %d\n",x);
+ printf("the vlue of x is: %d\n",*(&x));
  printf("the vlue of x is: %d\n",*p);
+ 
+ printf("Add of x is:%p\n",&x);
+ printf("add of x is:%p\n",p);
  
  printf("the add of p is:%p\n",&p);
  printf("the value of p is:%p\n",*(&p));
 
  
-
- 
     return 0;
 }
-
 
 
