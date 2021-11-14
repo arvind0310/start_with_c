6c7e1c7ec1f8402f02c7e27e5c9c473f15b1c210
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-11-14 21:09:50 +0530

    Create calloc.c

---
+++ Dynamic Memory Allocation/calloc.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr;
+    int n;
+    printf("how many integer you want?:");
+    scanf("%d",&n);
+    
+    ptr=(int *)calloc(n,sizeof(int));
+    
+    // for(int i=0;i<n;i++)
+    
+    // {
+    //     printf("enter the %d elemnt: ",i);
+    //     scanf("%d",&ptr[i]);
+    // }
+
+     for(int i=0;i<n;i++)
+    {
+        printf("element %d is %d\n",i,ptr[i]);
+    }
+
+    return 0;
+}
+
+// >>> automatically allocate the given space to : 0
