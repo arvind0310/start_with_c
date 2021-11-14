55f7704d94caf304966726d9e8f288aa535cc188
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-11-14 13:31:59 +0530

    Update char_array.c

--- char array and string/char_array.c
+++ char array and string/char_array.c
@@ -140,3 +140,20 @@ int main()
   
 }
 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <stdio.h>
+
+int main () {
+   char str[50];
+
+   printf("Enter a string : ");
+   gets(str); // will take string with spaces in a single line input
+
+   printf("You entered: %s", str);
+
+   return(0);
+}
+
+
