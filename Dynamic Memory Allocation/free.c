61b7dce18fa8e4209a98894e83e9759cb88c035a
Arvind Yadav <73684372+arvind0310@users.noreply.github.com>
2021-11-14 21:41:11 +0530

    Update free.c

--- Dynamic Memory Allocation/free.c
+++ Dynamic Memory Allocation/free.c
@@ -1,3 +1,37 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr;
+    int *ptr2;
+    ptr=(int *)malloc(3*sizeof(int)); // now ptr can be used as arr of size "6". 
+    
+    for(int i=0;i<3;i++)
+    
+    {
+        ptr2=(int *)malloc(400*sizeof(int)); // every time it will use the memory space of 460 intregres 
+        
+        printf("enter the %d elemnt: ",i);
+        scanf("%d",&ptr[i]);
+        
+        free(ptr2);     // but at the same time we are making it free ... (make .. free and make free and loop)
+    }
+
+     for(int i=0;i<3;i++)
+    {
+        printf("element %d is %d\n",i,ptr[i]);  // print up to 6 element
+    }
+    
+
+    return 0;
+}
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #include<stdio.h>
 #include<stdlib.h>
 
